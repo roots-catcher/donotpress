1d4980d4b56db7c02a8ea534d1fd120a52a44f1d
roots_catcher <yurkor4@gmail.com>
2022-11-23 22:55:11 +0300

    hosting

--- public/js/myfuckingsite.js
+++ public/js/myfuckingsite.js
@@ -4,13 +4,55 @@ document.addEventListener("DOMContentLoaded", () => {
           dangerBtn = openpage.querySelector('button.danger'),
           dude = openpage.querySelector('.dude');
 
+    let i = 0;
 
     dangerBtn.addEventListener('click', (event) => {
         event.preventDefault();
         
-            dangerBtn.classList.remove('active');
-            dude.classList.add('active');
-            document.querySelector('body').classList.add('bomb');
+            switch (i) {
+                case 0:
+                    dangerBtn.style.marginTop = "20%";
+                    dangerBtn.style.marginLeft = "43%";
+                    dangerBtn.style.fontSize = "55px";
+                    i++;
+                    break;
+                case 1:          
+                    dangerBtn.innerHTML = 'НЕ<br> ЖМИ<br> СЮДА!';
+                    i++;
+                    break;
+                case 2:
+                    let answer = prompt("Ты идиот?", "Да/Нет").toLowerCase();
+                    if (answer == "да") {
+                        alert("Нет...");
+                        dangerBtn.classList.remove('active');
+                        dude.classList.add('active');
+                        document.querySelector('body').classList.add('bomb');
+                        break;
+                    } else if (answer == "нет") {
+                        alert("ТОГДА НЕ ТРОГАЙ КНОПКУ!!!");
+                        location.reload();
+                    } else if (answer == "мне потанцевать нельзя?" || answer == "мне что потанцевать нельзя?" 
+                    || answer == "мне потанцевать нельзя" || answer == "я же просто танцую" ) {
+                        console.log("redirect");
+                        location.href = "https://www.youtube.com/watch?v=zdynIWXtdr0&ab_channel=MKScongratzzz";
+                    } else {
+                        alert("Не попал");
+                        i++;
+                    }
+                    console.log(answer);
+                    break;
+                case 3:
+                    dangerBtn.classList.remove('active');
+                    dude.classList.add('active');
+                    document.querySelector('body').classList.add('bomb');
+                    break;
+
+                    //location.reload();
+            }
+        console.log(i);
+            
+
+      
     });        
     
 
